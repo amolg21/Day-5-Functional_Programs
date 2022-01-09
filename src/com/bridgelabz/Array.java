3cd58a68680d2749e8769917bcbb6204d3137073
Amol Ghotale <amol9.ag@gmail.com>
2022-01-09 16:22:30 +0530

    Added Array Problem

---
+++ src/com/bridgelabz/Array.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Array {
+
+    public static int[][] arr;
+
+    public static void main(String[] args) {
+        // Making a 2D Array
+        int a;
+        arr = new int[2][3];     // A array with 2 rows and 3 columns.
+        Scanner sc = new Scanner(System.in);
+        // Displaying the array.
+        for (int i = 0; i < 2; i++) {
+
+            for (int j = 0; j < 3; j++) {
+
+                System.out.println("Enter the value");
+                a = sc.nextInt();
+                arr[i][j] = a;
+                //System.out.print(array[i][j]);
+            }
+
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
